99f1fd4e59f5a7246c58dc451bca496441dd81f8
osaichuk <oleksandr.saichuk@gmail.com>
2018-03-05 03:35:58 +0200

    change gulpfile delete browserSynk

--- hw_gulp_sass/gulpfile.js
+++ hw_gulp_sass/gulpfile.js
@@ -1,25 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
 
 gulp.task('sass', function() {
 	return gulp.src('app/sass/*.scss')
 		.pipe(sass())
-		.pipe(gulp.dest('app/css/'))
-		.pipe(browserSync.reload({
-			stream: true
-		}));
+		.pipe(gulp.dest('app/css/'));
 });
 
-gulp.task('watch', ['browserSync', 'sass'], function() {
+gulp.task('watch', ['sass'], function() {
 	gulp.watch('app/sass/*.scss', ['sass']);
-});
-
-gulp.task('browserSync', function() {
-	browserSync({
-		server: {
-			baseDir: 'app'
-		}
-	});
-
 });
