da875529d23474abcab7545c15eeabcfee5303e3
osaichuk <oleksandr.saichuk@gmail.com>
2018-03-27 02:48:08 +0300

    hw2602

---
+++ hw2602/02/02.js
@@ -0,0 +1,5 @@
+var firstNumber = prompt('Введите первую цифру');
+var secondNumber = prompt('Введите вторую цифру');
+var result = +firstNumber + +secondNumber;
+alert(result);
+
---
+++ hw2602/05/05.js
@@ -0,0 +1,3 @@
+var firstNumber = prompt('Введите первую цифру');
+var secondNumber = prompt('Введите вторую цифру');
+var result = +firstNumber + +secondNumber; alert(result);
